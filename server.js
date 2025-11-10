ca200bf1a8ee502840e359e83029dcb9a67f3072
mlmartin44 <mlmarti@pointpark.edu>
2025-11-09 19:42:37 -0500

    filter on the ID (working)

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { connectToDb, getDb } = require('./db');
+const { ObjectId } = require('mongodb')
 
 const app = express();
 
@@ -30,6 +31,32 @@ app.get('/movies', (req, res) => {
       res.status(500).json({ error: 'Could not fetch documents' });
     });
 });
+
+// GET /movies/:id  -> return one movie by _id
+app.get('/movies/:id', (req, res) => {
+  if (!db) return res.status(503).json({ error: 'Database not connected yet. Try again in a moment.' });
+
+  const id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'Invalid ObjectId format' });
+  }
+
+  const results = [];
+  db.collection('movies')
+    .find({ _id: new ObjectId(id) })   // find by _id
+    .forEach(doc => results.push(doc)) // collect (should be 0 or 1)
+    .then(() => {
+      if (results.length === 0) {
+        return res.status(404).json({ error: 'Movie not found' });
+      }
+      res.status(200).json(results[0]); // return single doc
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Error fetching document' });
+    });
+});
+
 
 // CONNECT TO DB, THEN START SERVER
 connectToDb((err) => {
