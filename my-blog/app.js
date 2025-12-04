9360b2abe2a618fe7c6250217f267db01bf209dd
mlmartin44 <mlmarti@pointpark.edu>
2025-12-03 19:48:28 -0500

    Blog code tested and working

---
+++ my-blog/app.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Post = require("./models/Post");
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
+
+
+// Routes
+
+// Home page – list all posts
+app.get("/", async (req, res) => {
+  const posts = await Post.find().sort({ createdAt: -1 });
+  res.render("index", { posts });
+});
+
+// Show form to create a new post
+app.get("/posts/new", (req, res) => {
+  res.render("newPost");
+});
+
+// Handle form submission and create post
+app.post("/posts", async (req, res) => {
+  const { title, content } = req.body;
+  await Post.create({ title, content });
+  res.redirect("/");
+});
+
+// Single post page
+app.get("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("post", { post });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
